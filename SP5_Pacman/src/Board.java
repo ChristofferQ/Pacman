a9819ce84c298d77beb28f33d7c89f7eac3b1e47
ChristofferQ <71074785+ChristofferQ@users.noreply.github.com>
2020-12-07 12:34:03 +0100

    123

---
+++ SP5_Pacman/src/Board.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Board extends PApplet
+{
+
+//    public void drawBoard()
+//    {
+//        noFill();
+//        stroke(128, 128, 255);
+//        for (int j = 0; j < height / 20; j++)
+//        {
+//            for (int i = 0; i < width / 20; i++)
+//            {
+//                rect(20 * i, 20 * j, 20, 20);
+//            }
+//        }
+//    }
+
+}
--- SP5_Pacman/src/Main.java
+++ SP5_Pacman/src/Main.java
@@ -2,25 +2,37 @@ import processing.core.PApplet;
 
 public class Main extends PApplet
 {
-
-    public void settings()
-    {
-        size(800,800);
-        background(0);
-    }
-
-    public void setup()
-    {
-
-    }
-
-    public void draw()
-    {
-
-    }
-
+//    public static PApplet processing;
     public static void main(String[] args)
     {
         PApplet.main("Main");
     }
+
+    public void settings()
+    {
+        size(600,800);
+    }
+
+    public void setup()
+    {
+        background(0);
+//        processing = this;
+    }
+
+    public void draw()
+    {
+//        Board myBoard = new Board();
+//        myBoard.drawBoard();
+        noFill();
+        stroke(128, 128, 255);
+        for (int j = 0; j < height / 20; j++)
+        {
+            for (int i = 0; i < width / 20; i++)
+            {
+                rect(20 * i, 20 * j, 20, 20);
+            }
+        }
+    }
+
+
 }
